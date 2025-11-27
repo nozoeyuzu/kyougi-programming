ebd9c61e88be8a199f16b471669b3bbb917270b4
nozoeyuzu <citron12yz21n82@docomo.ne.jp>
2025-11-27 10:20:45 +0900

    1127

---
+++ 20251127_atcoder.py
@@ -0,0 +1,42 @@
+#ABC432
+# A, B, C = map(int, input().split())
+# ans = []
+# if A>B and A>C:
+#     ans.append(A)
+#     if B > C:
+#         ans.append(B)
+#         ans.append(C)
+#     else:
+#         ans.append(C)
+#         ans.append(B)
+# elif B>A and B>C:
+#     ans.append(B)
+#     if A > C:
+#         ans.append(A)
+#         ans.append(C)
+#     else:
+#         ans.append(C)
+#         ans.append(A)
+# else:
+#     ans.append(C)
+#     if A > B:
+#         ans.append(A)
+#         ans.append(B)
+#     else:
+#         ans.append(B)
+#         ans.append(A)
+# print(''.join(map(str,ans)))
+
+# A,B,C = map(int, input().split())
+# ans = sorted([A, B, C], reverse = True)
+# print(''.join(map(str, ans)))
+
+#0以外をソートして最後に最上位桁の後ろに0を挿入する。
+# X = list(map(int, input().strip()))
+# non_zero = [n for n in X if n!=0]
+# zeros = [n for n in X if n==0]
+
+# non_zero.sort()
+# ans = non_zero[:1] + zeros + non_zero[1:]
+# print(''.join(map(str,ans)))
+

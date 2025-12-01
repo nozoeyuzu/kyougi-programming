f046a2576d6c5438b74c01f37203dc61df50f2b5
nozoeyuzu <citron12yz21n82@docomo.ne.jp>
2025-12-01 22:16:23 +0900

    398

--- Bmondai.py
+++ Bmondai.py
@@ -1,18 +1,39 @@
-S = list(map(str, input().strip()))
-expected = 'i'
-position = 0
-ans = 0
+# S = list(map(str, input().strip()))
+# expected = 'i'
+# position = 0
+# ans = 0
 
-for _ in range(len(S)):
-    if S[position] == expected:
-        position += 1
-        if expected == 'i':
-            expected = 'o'
-        else:
-            expected = 'i'
-    else:
-        position += 1
-        ans += 1
-if (ans + len(S))%2:
-    ans += 1
-print(ans)
+# for _ in range(len(S)):
+#     if S[position] == expected:
+#         position += 1
+#         if expected == 'i':
+#             expected = 'o'
+#         else:
+#             expected = 'i'
+#     else:
+#         position += 1
+#         ans += 1
+# if (ans + len(S))%2:
+#     ans += 1
+# print(ans)
+
+A = list(map(int, input().split()))
+two_count = 0
+three_count = 0
+dictionary_A = {}
+
+for i in range(len(A)):
+    if A[i] not in dictionary_A:
+        dictionary_A[A[i]] = 0
+    dictionary_A[A[i]] += 1
+
+for v in dictionary_A.values():
+    if v >= 2:
+        two_count += 1
+    if v >= 3:
+        three_count += 1
+
+if three_count >= 1 and two_count >= 2:
+    print('Yes')
+else:
+    print('No')

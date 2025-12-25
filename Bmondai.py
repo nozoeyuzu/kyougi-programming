59a5dbe9cb5d9162899d35303ae8174b96552ace
nozoeyuzu <citron12yz21n82@docomo.ne.jp>
2025-12-25 16:07:25 +0900

    422

--- Bmondai.py
+++ Bmondai.py
@@ -338,3 +338,19 @@
 #    a.append(int(str(a[i-1]+a[i-2])[::-1]))
 # print(a[9])
 
+H, W = map(int, input().split())
+S = [input().strip() for _ in range(H)]
+
+for i in range(H):
+    for j in range(W):
+        if S[i][j] == '#':
+            count = 0
+            for di, dj in [(1,0), (-1,0), (0,1), (0,-1)]:
+                k = i + di
+                l = j + dj
+                if 0<= k < H and 0 <= l < W and S[k][l] == '#' :
+                    count += 1
+            if count != 2 and count != 4:
+                print('No')
+                exit()
+print('Yes')
